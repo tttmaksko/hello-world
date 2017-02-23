9083f977cf0e041337599edf20d98a87ec70772b
tttmaksko <tttmaksko@mail.ru>
2017-02-23 15:54:44 +0300

    Try to fix missing CMakeLists.txt

--- .travis/run.sh
+++ .travis/run.sh
@@ -17,6 +17,6 @@ fi
 
 mkdir build && cd build
 conan install ../hello -s build_type=$BUILD_TYPE --build=missing
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER
+cmake ../hello -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER
 cmake --build . -- VERBOSE=1
 ctest -V
